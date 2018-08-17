8bd2ab0184529da49e5e06ebb60d16ade09f60ba
Dermot <dermot.hughes@rewardinsight.com>
2018-08-17 16:45:49 +0100

    Added image download functionality

--- scrape.js
+++ scrape.js
@@ -1,10 +1,10 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const download = require('image-downloader')
 const fs = require('fs');
 const writeStream = fs.createWriteStream('export.csv'); //
-console.log('Creating file...');
-//Write Headers
-writeStream.write(`src \n`)
+
+writeStream.write(`src \n`) //Write Headers
 
 const url = 'http://unsplash.com'; // URL you want to scrape
 console.log('Scraping...');
@@ -13,12 +13,35 @@ request(url, (error, response, html) => {
         const $ = cheerio.load(html);
         $('img').each((i, el) => { // Loops through each img
             const item = $(el).attr('src'); // Looks for src attribute
+            let file = i;
             if (item !== undefined) {
                 writeStream.write(item + '\n'); // Writes to new line in csv
             }
+            //TO DO Add Downloader
+
+            const dir = 'download'; // Folder where images are downloaded
+
+            const options = {
+                url: item,
+                dest: `./${dir}/${file}.jpg`                  // Save to /path/to/dest/image.jpg
+            }
+
+            if (!fs.existsSync(dir)) { // Checks if folder exists and if not, then creates it
+                fs.mkdirSync(dir);
+            }
+
+            download.image(options)
+                .then(({ filename, image }) => {
+                    console.log('File saved to', filename)
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
         });
         console.log('Scrape done');
+
     } else {
         console.log(error);
     }
 });
+
